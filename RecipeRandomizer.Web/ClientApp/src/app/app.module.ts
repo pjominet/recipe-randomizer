3cc61584f2e302b0ab0f5fa0e0f15e95c5bdac27
Patrick Jominet <patrick.jominet@technologypartner.io>
2020-08-10 10:35:47 +0200

    migrated to angular 9

--- RecipeRandomizer.Web/ClientApp/src/app/app.module.ts
+++ RecipeRandomizer.Web/ClientApp/src/app/app.module.ts
@@ -1,18 +1,13 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {LOCALE_ID, NgModule} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {AppRouting} from './app.routing';
 
-// localization
-import {registerLocaleData} from '@angular/common';
-import localEn from '@angular/common/locales/en-DE'
 
 // views
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
-
-registerLocaleData(localEn);
 
 @NgModule({
     declarations: [
@@ -25,9 +20,7 @@ registerLocaleData(localEn);
         FormsModule,
         AppRouting
     ],
-    providers: [
-        {provide: LOCALE_ID, useValue: 'en-DE'},
-    ],
+    providers: [],
     bootstrap: [AppComponent]
 })
 export class AppModule {
