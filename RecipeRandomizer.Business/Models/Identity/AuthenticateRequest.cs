336d996876366749bed2098c953b7febd7fb8c4e
Patrick Jominet <patrick.jominet@technologypartner.io>
2020-08-17 17:59:31 +0200

    minor simplification

--- RecipeRandomizer.Business/Models/Identity/AuthenticateRequest.cs
+++ RecipeRandomizer.Business/Models/Identity/AuthenticateRequest.cs
@@ -1,13 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace RecipeRandomizer.Business.Models.Identity
+﻿namespace RecipeRandomizer.Business.Models.Identity
 {
     public class AuthenticateRequest
     {
-        [Required]
         public string Email { get; set; }
-
-        [Required]
         public string Password { get; set; }
     }
 }
