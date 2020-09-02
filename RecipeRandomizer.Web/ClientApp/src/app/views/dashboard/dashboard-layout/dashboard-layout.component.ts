1576f448530d3e7a208c33be4ef6278b68ac7889
Patrick Jominet <patrick.jominet@technologypartner.io>
2020-09-02 09:53:45 +0200

    added custom scrollbar

--- RecipeRandomizer.Web/ClientApp/src/app/views/dashboard/dashboard-layout/dashboard-layout.component.ts
+++ RecipeRandomizer.Web/ClientApp/src/app/views/dashboard/dashboard-layout/dashboard-layout.component.ts
@@ -1,12 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
 
 @Component({
     selector: 'app-dashboard-layout',
     templateUrl: './dashboard-layout.component.html',
     styleUrls: ['./dashboard-layout.component.scss']
 })
-export class DashboardLayoutComponent {
+export class DashboardLayoutComponent implements OnInit {
 
-    constructor() {
+    constructor(private router: Router) {
+    }
+
+    public ngOnInit(): void {
+        this.router.events.subscribe((evt) => {
+            if (!(evt instanceof NavigationEnd)) {
+                return;
+            }
+            window.scrollTo(0, 0)
+        });
     }
 }
