c0febadb75af99bd67a796eb3e7d678b59f5ada6
Patrick Jominet <patrick.jominet@technologypartner.io>
2020-09-04 18:54:58 +0200

    custom labels now possible on file upload component

--- RecipeRandomizer.Web/ClientApp/src/app/components/file-upload/file-upload.component.ts
+++ RecipeRandomizer.Web/ClientApp/src/app/components/file-upload/file-upload.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, TemplateRef, ViewChild} from '@angular/core';
 
 @Component({
     selector: 'app-file-upload',
