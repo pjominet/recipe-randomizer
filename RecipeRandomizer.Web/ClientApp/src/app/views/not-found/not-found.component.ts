6d8232158ecb1ec05fbd230602fab0ece30c1249
Patrick Jominet <patrick.jominet@technologypartner.io>
2020-09-04 11:31:59 +0200

    added 404 page

--- RecipeRandomizer.Web/ClientApp/src/app/views/not-found/not-found.component.ts
+++ RecipeRandomizer.Web/ClientApp/src/app/views/not-found/not-found.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {Location} from '@angular/common';
 
 @Component({
-  selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+    selector: 'app-not-found',
+    templateUrl: './not-found.component.html',
+    styleUrls: ['./not-found.component.scss']
 })
-export class NotFoundComponent implements OnInit {
+export class NotFoundComponent {
 
-  constructor() { }
+    constructor(private location: Location) {
+    }
 
-  ngOnInit(): void {
-  }
-
+    public historyBack(): void {
+        this.location.back();
+    }
 }
