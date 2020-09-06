5649521f62bed1cbed486ac50d8d05421827af96
Patrick Jominet <patrick.jominet@technologypartner.io>
2020-09-07 00:02:25 +0200

    prettified main page, added cookie info, closes #2

--- RecipeRandomizer.Web/ClientApp/src/app/views/home/home.component.ts
+++ RecipeRandomizer.Web/ClientApp/src/app/views/home/home.component.ts
@@ -3,9 +3,12 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
-    styleUrls: []
+    styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+
+    public showCookiesInfo: boolean = true;
+
     constructor() {
     }
 }
