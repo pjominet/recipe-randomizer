aad488e96d11c488926a613a3b1a3e139448396f
Patrick Jominet <patrick.jominet@technologypartner.io>
2020-09-07 00:36:13 +0200

    more prettification and proper cookie message handling

--- RecipeRandomizer.Web/ClientApp/src/app/views/home/home.component.ts
+++ RecipeRandomizer.Web/ClientApp/src/app/views/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {CookieService} from 'ngx-cookie-service';
 
 @Component({
     selector: 'app-home',
@@ -9,6 +10,12 @@ export class HomeComponent {
 
     public showCookiesInfo: boolean = true;
 
-    constructor() {
+    constructor(private cookieService: CookieService) {
+        this.showCookiesInfo = this.cookieService.get('acceptedCookies') != 'true';
+    }
+
+    public acceptCookies(): void {
+        this.showCookiesInfo = false;
+        this.cookieService.set('acceptedCookies', 'true', 180);
     }
 }
